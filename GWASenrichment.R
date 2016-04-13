759fb1ba817fcbbc19c903b9338764a38d6f3e7d
Claire Green <cmgreen1@sheffield.ac.uk>
2016-04-13 15:53:51 +0100

    13_04_16

--- GWASenrichment.R
+++ GWASenrichment.R
@@ -1,35 +1,35 @@
-setwd(dir = "/Users/clairegreen/Documents/PhD/TDP-43/TDP-43_Data/")
 
-GWAScen <- read.table(file = "signif.snp.GWAScentral.p0.0001.1.txt")
-GWAScen <- GWAScen$V1
+#Load database of associations
+setwd(dir = "/Users/clairegreen/Documents/PhD/TDP-43/TDP-43_Data/GWAS/")
 
-neuroX <- read.table(file = "signif.snp.NeuroX.p5E08.txt")
-neuroX <- neuroX$V1
+a <- read.table(file = "signif.snp.GWAScentral.txt")
+a <- a$V1
 
-GWAScatALS <- read.table(file = "ALSGWAScatalog.txt")
-GWAScatALS <- GWAScatALS$V1
+b <- read.table(file = "signif.snp.GWAScentral.p0.0001.1.txt")
+b <- b$V1
 
-setwd(dir = "/Users/clairegreen/Documents/PhD/TDP-43/TDP-43_Code/Results/Pathprint/")
-pathprint <- read.table(file = "Pathprintgenes.txt")
-pathprint <- pathprint$V1
+c <- read.table(file = "signif.snp.NeuroX.txt")
+c <- c$V1
 
-pathprintunique <- pathprint[!duplicated(pathprint)]
+d <- read.table(file = "signif.snp.NeuroX.p5E08.txt")
+d <- d$V1
 
+#load test file
 
+setwd (dir = "/Users/clairegreen/Documents/PhD/TDP-43/TDP-43_Data/GSEA/PCxN Example/")
+Z <- read.csv(file = "allgenes2.csv")
+y <- Z$RHO_PROTEIN_SIGNAL_TRANSDUCTION
 
-# pthresh <- 1.000e-05
-# 
-# gwasthresh <- subset(GWAScen, p.value <= pthresh)
-# 
-overlap <- Reduce(intersect, list(pathprint, GWAScatALS))
+#remove any duplicates
+y <- y[!duplicated(y)]
+
+#Intersect
+overlap <- Reduce(intersect, list(y, a))
 print(overlap)
 
 
 
-
-x <- read.table(file = "DEGs.txt")
-x <- x$V1
-
+#Load file with all genes
 library(hgu133plus2.db)
 sym <- hgu133plus2SYMBOL
 sym1 <- mappedkeys(sym)
@@ -38,14 +38,30 @@ sym3 <- data.frame (sym2)
 sym.probes <- names (sym2)
 sym.genes <- sym3[1,]
 
-x.in <- length (which(pathprintunique %in% neuroX))
-x.out <- length(pathprintunique) - x.in
-tot.in <- length (neuroX)
+#calculate counts
+
+# n <- max(length(a), length(b), length(c), length(d))
+# length(a) <- n
+# length(b) <- n
+# length(c) <- n
+# length(d) <- n
+# 
+# x <- cbind(a,b,c,d)
+# 
+# for (i in x[,1:4]) {
+
+x.in <- length (which(y %in% d))
+x.out <- length(y) - x.in
+tot.in <- length (d)
 tot.out <- length (sym.genes)
 
 counts <- matrix (nrow=2, ncol=2)
 counts [1,] <- c(x.in, tot.in)
 counts [2,] <- c(x.out, tot.out)
 
+#Conduct fisher's exact test for count data
+
 a5 <-fisher.test (counts)
 enrich <- a5$p
+print(enrich)
+
